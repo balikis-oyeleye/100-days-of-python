0255e7ff018aa247196e566080f31b7b55324a23
Ryu <96819668+balikis-oyeleye@users.noreply.github.com>
2024-01-01 16:11:33 +0100

    feat: initialize project

---
+++ day-1-to-day-10/day-1.py
@@ -0,0 +1,20 @@
+#  Write a Python program to print "Hello, World!" to the console.
+print("Day 1 - First day of python learning")
+print("Hello World!")
+print("*" * 10)
+
+
+# variables and inputs
+name = input("What is your name? ")
+favorite_color = input("What is your favorite color? ")
+
+print(name, 'likes', favorite_color)
+
+
+# Pounds to Kilograms conversion
+print("Pounds to kilogram conversion")
+
+# create a variable to store the user's weight
+weight_in_pounds = input("What is your weight?(in pounds) ")
+weight_in_kg =  str(int(weight_in_pounds) / 2.2)
+print(weight_in_kg + "kg" )
